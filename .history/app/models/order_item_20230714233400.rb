70b1e53809109e7dc7d97557b45eaf619ebbeedc
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-14 23:35:51 -0500

    added numericality= true validation to order_item model

--- .history/app/models/order_item_20230714233400.rb
+++ .history/app/models/order_item_20230714233400.rb
@@ -2,6 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  validates :quantity, :price, presence: true, :numericality => true
+  validates :quantity, :price, presence: true :numericality => true
   validates :quantity, numericality: { greater_than: 0 }
 end
