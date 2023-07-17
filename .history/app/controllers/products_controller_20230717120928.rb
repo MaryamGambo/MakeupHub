20fd1edaee3495d028ebe46c295bf6c200ce1e21
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-17 12:14:06 -0500

    Created a partial for products and added it to index, search and filter views

---
+++ .history/app/controllers/products_controller_20230717120928.rb
@@ -0,0 +1,46 @@
+class ProductsController < ApplicationController
+  # index page to view all products
+  def index
+    @products = Product.all
+  end
+  # show page to view individual product pages
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
+  # navigate through products by filters: types, on-sale,new,  and recently-updated
+  def filter
+    @products = Product.all
+
+    if params[:type_id].present?
+      @products = @products.where(type_id: params[:type_id])
+    end
+
+    if params[:new].present?
+      @products = @products.order(created_at: :desc)
+    end
+
+    if params[:recently_updated].present?
+      @products = @products.order(updated_at: :desc)
+    end
+
+    if params[:on_sale].present?
+      @products = @products.where(on_sale_status: true)
+    end
+  end
+
+
+  # search products by keywords and categories
+  def search
+    @products = Product.all
+
+    if params[:type_id].present? && params[:keyword].present?
+      @products = @products.where(type_id: params[:type_id])
+                           .where('name LIKE ?', "%#{params[:keyword]}%")
+    elsif params[:keyword].present?
+      @products = @products.where('name LIKE ?', "%#{params[:keyword]}%")
+    end
+
+  end
+end
