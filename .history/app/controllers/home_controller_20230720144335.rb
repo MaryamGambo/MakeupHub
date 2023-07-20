4a20982c0dace24fcab97d6e20ecb195f25d299d
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-20 17:10:02 -0500

    added content and styling to the home page

--- .history/app/controllers/home_controller_20230720144335.rb
+++ .history/app/controllers/home_controller_20230720144335.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @available_types = Type.all
+    @available_types = Type.all.sample(3)
     @on_sale_products = Product.where(on_sale_status: true).sample(3)
     @recently_updated_products = Product.order(updated_at: :desc).limit(3)
   end
