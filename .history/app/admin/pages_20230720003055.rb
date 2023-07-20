1be5de00d72bc0a0d31686ff4236a93a4d18b827
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-20 00:41:34 -0500

    renamed all page files from page to pages

--- .history/app/admin/pages_20230720003055.rb
+++ .history/app/admin/pages_20230720003055.rb
@@ -6,15 +6,6 @@ ActiveAdmin.register Page do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :title, :content
-
-  form do |f|
-    f.semantic_errors
-    f.inputs do
-      f.input :title
-      f.input :content, as: :text, input_html: { class: 'redactor' }
-    end
-    f.actions
-  end
   #
   # or
   #
