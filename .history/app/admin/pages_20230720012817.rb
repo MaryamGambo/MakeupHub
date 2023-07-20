1d7220dc9752b0816dc86a41a8dca71b1f66a035
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-20 01:29:48 -0500

    removed redactor from the pages form

---
+++ .history/app/admin/pages_20230720012817.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register Page do
+  config.filters = false
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :title, :content ,:permalink
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :title
+      f.input :content, as: :rich_text
+      f.input :permalink
+    end
+    f.actions
+  end
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :content]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
