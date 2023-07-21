033fc1b7c1897910af327e5193508c59c4f90bf2
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-20 23:13:01 -0500

    added a customer reference to the orders table

--- db/migrate/20230721030735_remove_customer_id_from_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCustomerIdFromOrders < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :orders, :customer_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_034257) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_035343) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -121,6 +121,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_034257) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "customer_id", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "pages", force: :cascade do |t|
@@ -179,6 +181,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_034257) do
   add_foreign_key "customers", "provinces", column: "primary_province_id"
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
+  add_foreign_key "orders", "customers"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "types"
