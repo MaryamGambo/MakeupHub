f3897353b6af82b6969c3e8a216dc2cc971a4787
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-07-27 21:42:06 -0500

    Registered customer, order and OrderItem models in Active admin

--- .history/app/admin/orders_20230727212857.rb
+++ .history/app/admin/orders_20230727212857.rb
@@ -7,27 +7,13 @@ ActiveAdmin.register Order do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :order_date, :GST, :HST, :PST, :total_amount, :status, :customer_id, :payment_intent_id
-
-  form do |f|
-    f.semantic_errors
-    f.inputs do
-      f.input :order_date
-      f.input :GST
-      f.input :HST
-      f.input :PST
-      f.input :total_amount
-      f.input :status, as: :select, collection: ['new', 'paid', 'shipped']
-      f.input :customer_id
-      f.input :payment_intent_id
-    end
-    f.actions
-  end
-
-
-
-
-
-
-
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:order_date, :GST, :HST, :PST, :total_amount, :status, :customer_id, :payment_intent_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
 
 end
